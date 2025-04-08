9c58ab2fd0445cb25fb5df1d6b08de09e2edd9df
Arman <100778134+Arimawn@users.noreply.github.com>
2025-04-08 20:11:32 +0330

    v1

---
+++ test.py
@@ -0,0 +1,9 @@
+# Step 1 => دریافت دیتا از کاربر
+base = input("Input Base: ")
+height = input("Input Height: ")
+
+# Step 2 => محاسبه
+S = (int(base) * int(height)) / 2
+
+# Step 3 => نمایش خروجی به کاربر
+print("Area Is: "+ str(S))
